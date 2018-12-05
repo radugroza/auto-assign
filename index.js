a4556834de6e6b98a46bc1b8d4ced1f913362043
Radu <radu.groza@bistone.eu>
2018-12-05 14:14:54 +0200

    Read from config file

--- index.js
+++ index.js
@@ -1,15 +1,34 @@
+const _ = require('lodash');
+
 module.exports = app => {
-  // Your code here
   app.log('Yay, the app was loaded!')
 
   app.on('pull_request.opened', async context => {
-    const p = context.payload;
+    const {owner, repo} = context.repo({path: '.github/auto-assign.yml'})
+    const p = context.payload
 
-    await context.github.issues.addAssignees({
-      owner: p.repository.owner.login,
-      repo: p.repository.name,
-      number: p.number,
-      assignees: ['radugroza', 'larisagroza']
-    });
-  });
-};
+    let cfg = await context.config('auto-assign.yml', {})
+    if (!cfg || !cfg.users) {
+      /* comment on the PR, no users could be found */
+      await context.github.issues.createComment({
+        owner: owner,
+        repo: repo,
+        number: p.number,
+        body: 'Could not auto-assign the PR because no users have been found. Make sure the **`.github/auto-assign.yml`** file exists and contains a `users` entry array'
+      })
+      return
+    }
+
+    /* randomly select a user and assign the PR */
+    if (!p.pull_request.assignees.length) {
+      cfg.users = cfg.users.filter((user) => user !== p.pull_request.user.login)
+
+      await context.github.issues.addAssignees({
+        owner: owner,
+        repo: repo,
+        number: p.number,
+        assignees: [_.sample(cfg.users)]
+      })
+    }
+  })
+}
